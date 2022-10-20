96e7ecc76064ed85d341b50ae359ce548984f5c2
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-10-20 14:18:07 +0300

    feat: add test api request

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { Footer } from '@components/footer/footer';
 import { Content } from '@components/content/content';
 import { useCssBaseline } from '@styles/hooks/useCssBaseline';
+import { ClientApp } from '../../api.client/client.app';
 import { useStyles } from './app.styles';
 import { Header } from '../header/header';
 
 export const App: FC = () => {
   const { root } = useStyles();
   useCssBaseline();
+
+  useEffect(() => {
+    const app = new ClientApp('http://localhost:8000/api');
+    app.testRequest();
+  });
 
   return (
     <div className={root}>
