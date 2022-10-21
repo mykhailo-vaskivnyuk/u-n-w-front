ba5b7ad4151bc67ea2e1f256958ebd7bc2fa61b7
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-10-21 11:36:51 +0300

    feat: merge changes

--- src/components/content/content.tsx
+++ src/components/content/content.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
 import { AuthForm } from '@components/forms/auth/auth';
+import { HashRouter } from 'react-router-dom';
 import { useStyles } from './content.styles';
 
 export const Content: FC = () => {
   const { root } = useStyles();
 
   return (
-    <div className={root}>
-      <AuthForm />
-    </div>
+    <HashRouter>
+      <div className={root}>
+        <AuthForm />
+      </div>
+    </HashRouter>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'react-jss';
 import { defaultTheme } from '@styles/theme';
 import { ErrorBoundary } from '@components/error/error.boundary';
 import { App } from './components/app/app';
 
-ReactDOM.render(
-  <ErrorBoundary level="top">
-    <ThemeProvider theme={defaultTheme}>
-      <ErrorBoundary level="app">
-        <App />
-      </ErrorBoundary>
-    </ThemeProvider>
-  </ErrorBoundary>,
-  document.getElementById('root'),
-);
+const root = document.getElementById('root');
+
+root &&
+  ReactDOM.createRoot(root).render(
+    <ErrorBoundary level="top">
+      <ThemeProvider theme={defaultTheme}>
+        <ErrorBoundary level="app">
+          <App />
+        </ErrorBoundary>
+      </ThemeProvider>
+    </ErrorBoundary>,
+  );
