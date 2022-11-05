6b8432197a4689c0c8a964ab99b3734b95d9b67b
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-11-05 23:08:37 +0200

    feat: update client api

--- src/api/common/api/client.api.ts
+++ src/api/common/api/client.api.ts
@@ -2,27 +2,46 @@ import * as P from './types';
 import * as Q from './client.api.types';
 
 export const getApi = (
-  fetch: <T>(pathname: string, options?: Record<string, any>) => Promise<T>,
+  fetch: <T>(pathname: string, options?: Record<string, any>) => Promise<T>
 ) => ({
-  account: {
-    confirm: (options: P.IConfirmParams) => fetch<P.IUserResponse>('/account/confirm', options),
-    login: (options: Q.TAccountLogin) => fetch<P.IUserResponse>('/account/login', options),
-    logout: () => fetch<boolean>('/account/logout'),
-    overmail: (options: P.ISignupParams) => fetch<boolean>('/account/overmail', options),
-    remove: () => fetch<boolean>('/account/remove'),
-    restore: (options: P.IConfirmParams) => fetch<P.IUserResponse>('/account/restore', options),
-    signup: (options: P.ISignupParams) => fetch<P.IUserResponse>('/account/signup', options),
+  'account': {
+    'confirm': (options: P.IConfirmParams) =>
+      fetch<P.IUserResponse>('/account/confirm', options),
+
+    'login': (options: Q.TAccountLogin) =>
+      fetch<P.IUserResponse>('/account/login', options),
+
+    'logout': () => fetch<boolean>('/account/logout'),
+
+    'overmail': (options: P.ISignupParams) =>
+      fetch<boolean>('/account/overmail', options),
+
+    'remove': () => fetch<boolean>('/account/remove'),
+
+    'restore': (options: P.IConfirmParams) =>
+      fetch<P.IUserResponse>('/account/restore', options),
+
+    'signup': (options: P.ISignupParams) =>
+      fetch<P.IUserResponse>('/account/signup', options),
+
   },
-  index: () => fetch<string>('/index'),
-  merega: {
-    read: () => fetch<Q.TMeregaReadResponse>('/merega/read'),
+  'index': () => fetch<string>('/index'),
+
+  'merega': {
+    'read': () => fetch<Q.TMeregaReadResponse>('/merega/read'),
+
   },
-  scripts: {
+  'scripts': {
     'script.js': () => fetch<Q.TScriptsScriptjsResponse>('/scripts/script.js'),
+
   },
-  user: {
-    create: (options: Q.TUserCreate) => fetch<Q.TUserCreateResponse>('/user/create', options),
-    update: () => fetch<string>('/user/update'),
-    read: () => fetch<P.IUserResponse>('/user/read'),
+  'user': {
+    'create': (options: Q.TUserCreate) =>
+      fetch<Q.TUserCreateResponse>('/user/create', options),
+
+    'update': () => fetch<string>('/user/update'),
+
+    'read': () => fetch<P.IUserResponse>('/user/read'),
+
   },
 });
