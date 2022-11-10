4e72f406ae4fc1e83ce7d8de077929f6901052fe
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-11-10 09:09:54 +0200

    feat: add checkConnection

--- src/components/app/useApp.ts
+++ src/components/app/useApp.ts
@@ -1,13 +1,8 @@
-// import { app } from '@api/app/client.app';
 import { app } from '@api/app/client.app';
-import { MessagesMap } from '@constants/messages';
-import { modalService } from '@services/modal.service';
 import { useEffect } from 'react';
 
 export const useApp = () => {
-  const showFailed = () => modalService.showError(MessagesMap.SERVER_ERROR);
-
   useEffect(() => {
-    app.init().catch(() => {});
+    app.init();
   }, []);
 };
