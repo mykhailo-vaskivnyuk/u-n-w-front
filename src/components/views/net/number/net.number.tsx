951a659a1d6d4b797bb35774bdd3bbc69c79c2e5
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2022-11-25 19:21:05 +0200

    feat: net.number

--- src/components/views/net/number/net.number.tsx
+++ src/components/views/net/number/net.number.tsx
@@ -4,6 +4,8 @@ import { vars } from '@styles/vars';
 import { ICONS } from '@components/icon/icon';
 import { IconButton } from '@components/buttons/icon.button/icon.button';
 import { NotFound } from '@components/views/not.found/not.found';
+import { Member } from '@components/member/member';
+// import { useSwap } from '@hooks/useSwap';
 import { useUserNet } from '../../../../hooks/useUserNet';
 import { useStyles } from './net.number.styles';
 
@@ -11,42 +13,33 @@ const USER_NET_POSITION = ['tree', 'circle'] as const;
 type UserNetPositionKeys = typeof USER_NET_POSITION[number];
 
 export const NetMain: FC = () => {
-  const {
-    container,
-    root,
-    menu,
-    member,
-    netView: clsNetView,
-    viewButton,
-    tree,
-    circle,
-  } = useStyles();
+  const { container, root, netView: clsNetView, viewButton, tree, circle } = useStyles();
   const [netView, setNetView] = useState<UserNetPositionKeys>('tree');
-
+  // const [netView, hendlers] = useSwap<UserNetPositionKeys>([...USER_NET_POSITION]);
   const notFound = useUserNet();
   if (notFound) return <NotFound />;
 
-  const membersTree = new Array(7).fill('tree').map((i, j) => (
-    <div key={`${i + j}`} className={member}>
-      {j === 0 ? 'current user' : `${i} member ${j}`}
-    </div>
-  ));
-  const membersCircle = new Array(7).fill('circle').map((i, j) => (
-    <div key={`${i + j}`} className={member}>
-      {j === 1 ? 'current user' : `${i} member ${j}`}
-    </div>
-  ));
+  const membersTree = new Array(7)
+    .fill('tree')
+    .map((i, j) => (
+      <Member key={`${i + j}`} name={j === 0 ? 'current user' : `${i} member ${j}`} />
+    ));
+
+  const membersCircle = new Array(7)
+    .fill('circle')
+    .map((i, j) => (
+      <Member key={`${i + j}`} name={j === 1 ? 'current user' : `${i} member ${j}`} />
+    ));
 
   return (
     <div className={container}>
-      <div className={menu}>member menu</div>
       <div
         className={root}
         style={{ left: netView === 'tree' ? 0 : `calc(-100% - ${vars.gap.main}` }}
       >
         <div className={clsx(clsNetView, tree)}>
           <IconButton
-            icon={ICONS.arrowLeft}
+            icon={ICONS.arrowRight}
             iconPosition="left"
             className={viewButton}
             onClick={() => setNetView('circle')}
